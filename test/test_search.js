465b064560c57ba46f9b9cef1f2ebf96cf7e6b5c
DemoHn <601790183@qq.com>
2014-03-02 23:48:57 +0800

    add trim method to delete reluctent spaces and returns

--- test/test_search.js
+++ test/test_search.js
@@ -5,7 +5,16 @@ var cc98 = new _98();
 cc98.login(function(data){
   if(data.status === 1){
     cc98.searchPosts("fefefefea",1,{},function(data){
+      console.log("No1:");
       console.log(data);
+      //404
     });
+
+    cc98.searchPosts("cc98",1,{},function(data){
+      console.log("No1:");
+      console.log(data);
+      //with results
+    });
+
   }
 });
