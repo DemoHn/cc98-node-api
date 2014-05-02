9e01a3a416e9f2bff74ddef2faa5595031d7e5eb
demohn <demohn@foxmail.com>
2014-05-02 10:13:22 +0800

    handle error?

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var cc98 = require("./lib/basic");
 var config = {
 
     defaults:{
-      root:"www.cc98.org",
+ //     root:"www.cc98.org",
+	root:"db.zju.edu.cn/cc98",
       "User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36"
     },
     CookieFile:"LOGIN_COOKIE"
