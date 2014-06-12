91e229051ed95603971a1f8f3d03c42164eb1810
demohn <demohn@foxmail.com>
2014-06-12 09:53:44 +0100

    update doc

--- config.js
+++ config.js
@@ -3,10 +3,8 @@ var cc98 = require("./lib/basic");
 var config = {
 
     defaults:{
- //     root:"www.cc98.org",
-	root:"db.zju.edu.cn/cc98",
+      root:"www.cc98.org",
       "User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36"
-    },
-    CookieFile:"LOGIN_COOKIE"
+    }
 };
 module.exports = config;
