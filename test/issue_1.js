c432b15281be4ab70a188bf1f2fa0baa7d057c18
DemoHn <601790183@qq.com>
2014-08-14 23:53:48 +0530

    add match time

--- test/issue_1.js
+++ test/issue_1.js
@@ -4,7 +4,7 @@ var uc = require("./user_config");
 var cc98 = new _98(uc);
 
 cc98.login(function(){
-    cc98.getPostInfo(357,4411365,1,1,function(data){
+    cc98.getPostInfo(537,4412816,1,1,function(data){
         console.log(data[1].list);
     });
 })
