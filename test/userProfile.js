c4b1b2ef1f9e2d46e23211a7b6b282ad72c4808a
DemoHn <601790183@qq.com>
2014-09-07 06:05:47 +0100

    encode

--- test/userProfile.js
+++ test/userProfile.js
@@ -4,40 +4,39 @@ var uc = require("./user_config");
 var cc98 = new _98(uc);
 
 var profile = {
-  title:"233",
-  Sex:"1",
-  birthyear:"1995",
-  birthmonth:"10",
-  birthday:"12",
-  face:"",
-  myface:"http://file.cc98.org/uploadface/440318.jpg",
-  width:86,
-  height:120,
-  userphoto:"hhhhh",
-  groupname:"doubi.js",
-  Signature:"233333",
-  homepage:"http://www.demohn.com",
-  Email:"demohn@foxmail.com",
-  OICQ:"601790183",
-  msn:"",
-  usercookies:4,
-  ShowMedal:"1",
-  ShowLife:"1",
-  AutoPlay:"1",
-  EnableOWA:"1",
-  setuserinfo:"1",
-  usermsgring:"1",
-  sendring:0,
-  EnablePopup:1
+  title: "233",
+  Sex: "1",
+  birthyear: "1995",
+  birthmonth: "10",
+  birthday: "12",
+  face: "",
+  myface: "http://file.cc98.org/uploadface/440318.jpg",
+  width: 86,
+  height: 120,
+  userphoto: "hhhhh",
+  groupname: "doubi.js",
+  Signature: "233333",
+  homepage: "http://www.demohn.com",
+  Email: "demohn@foxmail.com",
+  OICQ: "601790183",
+  msn: "",
+  usercookies: 4,
+  ShowMedal: "1",
+  ShowLife: "1",
+  AutoPlay: "1",
+  EnableOWA: "1",
+  setuserinfo: "1",
+  usermsgring: "1",
+  sendring: 0,
+  EnablePopup: 1
 };
 
-cc98.login(function(data){
-  cc98.readMyProfile(function(data){
-//    console.log("\nTEST 01: readMyIndex:\n");
-//    console.log(data);
+cc98.login(function(data) {
 
-    cc98.readUserProfile("name","DHL快递",function(data){
+  cc98.getAllPostList(100, 1, 1, function(data) {
+    var dl = data[1]["list"];
+    cc98.readUserProfile("name", dl[4]["author"], function(data) {
       console.log(data);
     });
-  });
+  })
 });
