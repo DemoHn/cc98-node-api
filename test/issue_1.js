7e34c4f45a033411de722c9a32666eba679562a6
DemoHn <601790183@qq.com>
2014-09-12 00:37:50 +0800

    跟着樱桃做抓取规则的修改～～（把blockquote标签改成article标签）

--- test/issue_1.js
+++ test/issue_1.js
@@ -4,8 +4,8 @@ var uc = require("./user_config");
 var cc98 = new _98(uc);
 
 cc98.login(function(){
-    cc98.getPostInfo(537,4412816,1,1,function(data){
-      //  console.log(data[1].list);
+    cc98.getPostInfo(537,4420101,1,1,function(data){
+        console.log(data[1].list);
     });
 
     /*cc98.uploadFile("a.gif",198,function(msg,url){
