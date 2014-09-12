a0ff73a7113202ab3a53316f86e7a8e7be08ba4e
DemoHn <601790183@qq.com>
2014-09-12 23:45:51 +0800

    add watchedBoard module

--- test/customBoard.js
+++ test/customBoard.js
@@ -4,9 +4,13 @@ var cc98 = new _98(uc);
 
 cc98.login(function(data){
     if(data.status == 1){
+
+      cc98.watchedBoard(function(data){
+        console.log(data);
+      });
         cc98.customizeBoard(283,function(body,status){
             console.log(body);
-            console.log(status);            
+            console.log(status);
         })
     }
 });
